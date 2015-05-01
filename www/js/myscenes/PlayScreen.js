2271854b08d184d1ec4aa9a884d4cc344d41f36a
raphaelstary <raphael.stary@gmail.com>
2015-05-01 23:18:47 +0200

    reformatted code

--- www/js/myscenes/PlayScreen.js
+++ www/js/myscenes/PlayScreen.js
@@ -26,9 +26,11 @@ var PlayScreen = (function () {
         var points = this.stage.drawText(Width.get(32, 28), Height.get(48, 3), '0', Font._15, 'GameFont', '#fc6da4');
         drawables.push(points);
 
-        var turnLeft = this.stage.drawFresh(Width.get(32, 4), Height.get(48, 44), 'left', undefined, undefined, undefined, undefined, 3);
+        var turnLeft = this.stage.drawFresh(Width.get(32, 4), Height.get(48, 44), 'left', undefined, undefined,
+            undefined, undefined, 3);
         drawables.push(turnLeft);
-        var turnRight = this.stage.drawFresh(Width.get(32, 28), Height.get(48, 44), 'right', undefined, undefined, undefined, undefined, 3);
+        var turnRight = this.stage.drawFresh(Width.get(32, 28), Height.get(48, 44), 'right', undefined, undefined,
+            undefined, undefined, 3);
         drawables.push(turnRight);
 
         var outerCircle = this.stage.drawCircle(Width.HALF, Height.HALF, Font._10, BUBBLE_GUM, false, Font._35);
@@ -36,10 +38,13 @@ var PlayScreen = (function () {
         var innerCircle = this.stage.drawCircle(Width.HALF, Height.HALF, Font._30, LEMONADE, true);
         drawables.push(innerCircle);
 
-        var cat1 = this.stage.drawFresh(Width.HALF, subtract(Height.HALF, outerCircle.getHeightHalf.bind(outerCircle)), 'white_cat', 4);
-        var cat2 = this.stage.drawFresh(Width.HALF, add(Height.HALF, outerCircle.getHeightHalf.bind(outerCircle)), 'black_cat', 4);
+        var cat1 = this.stage.drawFresh(Width.HALF, subtract(Height.HALF, outerCircle.getHeightHalf.bind(outerCircle)),
+            'white_cat', 4);
+        var cat2 = this.stage.drawFresh(Width.HALF, add(Height.HALF, outerCircle.getHeightHalf.bind(outerCircle)),
+            'black_cat', 4);
 
         var itIsOver = false;
+
         function toNextScene() {
             if (itIsOver)
                 return;
