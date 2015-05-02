76e4bff15f28919a1b4d1bfc75dac431dc91d979
raphaelstary <raphael.stary@gmail.com>
2015-05-02 15:26:15 +0200

    turn left & turn right

--- www/js/myscenes/PlayScreen.js
+++ www/js/myscenes/PlayScreen.js
@@ -6,6 +6,7 @@ var PlayScreen = (function () {
         this.events = services.events;
         this.timer = services.timer;
         this.tap = services.tap;
+        this.device = services.device;
     }
 
     var BUBBLE_GUM = '#fdd9e7';
@@ -26,22 +27,74 @@ var PlayScreen = (function () {
         var points = this.stage.drawText(Width.get(32, 28), Height.get(48, 3), '0', Font._15, 'GameFont', '#fc6da4');
         drawables.push(points);
 
-        var turnLeft = this.stage.drawFresh(Width.get(32, 4), Height.get(48, 44), 'left', undefined, undefined,
+        var left = this.stage.drawFresh(Width.get(32, 4), Height.get(48, 44), 'left', undefined, undefined, undefined,
+            undefined, 3);
+        drawables.push(left);
+        var right = this.stage.drawFresh(Width.get(32, 28), Height.get(48, 44), 'right', undefined, undefined,
             undefined, undefined, 3);
-        drawables.push(turnLeft);
-        var turnRight = this.stage.drawFresh(Width.get(32, 28), Height.get(48, 44), 'right', undefined, undefined,
-            undefined, undefined, 3);
-        drawables.push(turnRight);
+        drawables.push(right);
 
-        var outerCircle = this.stage.drawCircle(Width.HALF, Height.HALF, Font._10, BUBBLE_GUM, false, Font._35);
+        var radiusFn = Font._20;
+        var outerCircle = this.stage.drawCircle(Width.HALF, Height.HALF, radiusFn, BUBBLE_GUM, false, Font._35);
         drawables.push(outerCircle);
         var innerCircle = this.stage.drawCircle(Width.HALF, Height.HALF, Font._30, LEMONADE, true);
         drawables.push(innerCircle);
 
-        var cat1 = this.stage.drawFresh(Width.HALF, subtract(Height.HALF, outerCircle.getHeightHalf.bind(outerCircle)),
-            'white_cat', 4);
-        var cat2 = this.stage.drawFresh(Width.HALF, add(Height.HALF, outerCircle.getHeightHalf.bind(outerCircle)),
+        var topCat = this.stage.drawFresh(Width.HALF,
+            subtract(Height.HALF, outerCircle.getHeightHalf.bind(outerCircle)), 'white_cat', 4);
+        var bottomCat = this.stage.drawFresh(Width.HALF, add(Height.HALF, outerCircle.getHeightHalf.bind(outerCircle)),
             'black_cat', 4);
+
+        this.events.subscribe(Event.POINTER, function (pointer) {
+            if (pointer.type == 'down') {
+                if (pointer.x < Width.HALF(self.device.width)) {
+                    turnLeft();
+                } else {
+                    turnRight();
+                }
+            }
+        });
+
+        var speed = 15;
+        var turning = false;
+        var callbacks;
+
+        function turnLeft() {
+            if (turning)
+                return;
+            callbacks = 2;
+            turning = true;
+
+            self.stage.moveCircular(topCat, Width.HALF, Height.HALF, radiusFn, Math.PI / 2 * 3, Math.PI / 2, speed,
+                Transition.EASE_OUT_EXPO, false, endTurn);
+            self.stage.moveCircular(bottomCat, Width.HALF, Height.HALF, radiusFn, Math.PI / 2, -Math.PI / 2, speed,
+                Transition.EASE_OUT_EXPO, false, endTurn);
+            var pointer = bottomCat;
+            bottomCat = topCat;
+            topCat = pointer;
+        }
+
+        function endTurn() {
+            callbacks--;
+            if (callbacks <= 0) {
+                turning = false;
+            }
+        }
+
+        function turnRight() {
+            if (turning)
+                return;
+            callbacks = 2;
+            turning = true;
+
+            self.stage.moveCircular(topCat, Width.HALF, Height.HALF, radiusFn, -Math.PI / 2, Math.PI / 2, speed,
+                Transition.EASE_OUT_EXPO, false, endTurn);
+            self.stage.moveCircular(bottomCat, Width.HALF, Height.HALF, radiusFn, Math.PI / 2, Math.PI / 2 * 3, speed,
+                Transition.EASE_OUT_EXPO, false, endTurn);
+            var pointer = bottomCat;
+            bottomCat = topCat;
+            topCat = pointer;
+        }
 
         var itIsOver = false;
 
