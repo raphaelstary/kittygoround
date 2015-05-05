8ae83f035846126e52cfc4201848974738acc42c
raphaelstary <raphael.stary@gmail.com>
2015-05-05 15:55:45 +0200

    highscore

--- www/js/myscenes/EndScreen.js
+++ www/js/myscenes/EndScreen.js
@@ -1,4 +1,5 @@
-var EndScreen = (function (drawClouds, drawIcons, Width, Height, Font, drawButtons) {
+var EndScreen = (function (drawClouds, drawIcons, Width, Height, Font, drawButtons, loadInteger, localStorage,
+    Transition) {
     "use strict";
 
     function EndScreen(services) {
@@ -8,6 +9,10 @@ var EndScreen = (function (drawClouds, drawIcons, Width, Height, Font, drawButto
         this.messages = services.messages;
         this.timer = services.timer;
     }
+
+    var STORAGE_BEST = 'kitty_go_round-best';
+    var WHITE = '#fff';
+    var GOLD = '#ffd700';
 
     EndScreen.prototype.show = function (next) {
         var self = this;
@@ -21,11 +26,48 @@ var EndScreen = (function (drawClouds, drawIcons, Width, Height, Font, drawButto
             drawables.push(elem);
         });
 
-        var score = this.stage.drawText(Width.HALF, Height.get(48, 12), this.storage.points.toString(), Font._15,
+        var best = loadInteger(STORAGE_BEST);
+        var points = this.storage.points;
+        var newRecord = points > best;
+
+        if (newRecord)
+            localStorage.setItem(STORAGE_BEST, points);
+
+        var scoreTxt = this.stage.drawText(Width.HALF, Height.get(48, 12), this.messages.get('end', 'score'), Font._15,
             'GameFont', '#fc6da4');
-        drawables.push(score);
-        var pointsTxt = this.stage.drawText(Width.HALF, Height.get(48, 15), 'points', Font._15, 'GameFont', '#fc6da4');
-        drawables.push(pointsTxt);
+        drawables.push(scoreTxt);
+        var scoreValueTxt = this.stage.drawText(Width.HALF, Height.get(48, 15), points.toString(), Font._15, 'GameFont',
+            '#fc6da4');
+        drawables.push(scoreValueTxt);
+        var bestTxt = this.stage.drawText(Width.HALF, Height.get(48, 20), this.messages.get('end', 'best'), Font._15,
+            'GameFont', '#fc6da4');
+        drawables.push(bestTxt);
+        var bestValueTxt = this.stage.drawText(Width.HALF, Height.get(48, 23), this.storage.points.toString(), Font._15,
+            'GameFont', '#fc6da4');
+        drawables.push(bestValueTxt);
+
+        function getNewDistanceX() {
+            return bestTxt.getEndX() + bestTxt.getWidthHalf();
+        }
+
+        if (newRecord) {
+            var newScore = self.stage.drawText(getNewDistanceX, Height.get(48, 23),
+                self.messages.get('end', 'new_record'), Font._15, 'GameFont', GOLD, 3, [bestTxt]);
+            var newScoreHighlight = self.stage.drawText(getNewDistanceX, Height.get(48, 23),
+                self.messages.get('end', 'new_record'), Font._15, 'GameFont', WHITE, 4, [bestTxt]);
+            self.stage.animateAlphaPattern(newScoreHighlight, [
+                {
+                    value: 1,
+                    duration: 30,
+                    easing: Transition.LINEAR
+                }, {
+                    value: 0,
+                    duration: 30,
+                    easing: Transition.LINEAR
+                }
+            ], true);
+            drawables.push(newScore, newScoreHighlight);
+        }
 
         drawButtons(this.buttons, this.messages, this.timer, toNextScene).forEach(function (elem) {
             buttons.push(elem);
@@ -46,4 +88,4 @@ var EndScreen = (function (drawClouds, drawIcons, Width, Height, Font, drawButto
     };
 
     return EndScreen;
-})(drawClouds, drawIcons, Width, Height, Font, drawButtons);
+})(drawClouds, drawIcons, Width, Height, Font, drawButtons, loadInteger, lclStorage, Transition);
